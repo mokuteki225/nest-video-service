55b27c34ca79d6b10794a5ffa301e1df81f3c63e
Vlad Kondratiuk <kondratiuk011@gmail.com>
2022-04-27 01:11:44 +0300

    chore: install vital dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+import * as path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: path.join(process.cwd(), 'graphql/schema.gql'),
+    }),
+  ],
 })
 export class AppModule {}
